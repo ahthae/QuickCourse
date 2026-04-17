1167fcccc1dc1da5cc11bc15239577ec7c3c2c8d
ahthae <ahthaeon@pm.me>
2026-04-17 04:21:11 -0700

    Refine admin page views

--- back-end/quickcourse/admin.py
+++ back-end/quickcourse/admin.py
@@ -5,5 +5,28 @@ from quickcourse.models import Course, db, Student, StudentCourseAssociation
 
 admin = Admin(name="quickcourse")
 
-admin.add_view(ModelView(Course, db.session, name="Course", endpoint='course_admin'))
-admin.add_view(ModelView(Student, db.session, name="Student", endpoint='student_admin'))
+class CourseView(ModelView):
+    column_display_pk = True
+    column_searchable_list = ['crn', 'name']
+    column_editable_list = ['name', 'times', 'capacity']
+    column_formatters = {
+        'instructor': lambda v, c, m, n: m.instructor.name
+    }
+
+class StudentCourseView(ModelView):
+    column_editable_list = ['grade']
+    column_formatters = {
+        'student': lambda v, c, m, n: m.student.name,
+        'course': lambda v, c, m, n: m.course.name
+    }
+    column_formatters_detail = None
+
+class StudentView(ModelView):
+    column_searchable_list = ['id', 'name']
+    column_editable_list = ['username', 'name', 'role']
+    column_exclude_list = ['passhash']
+    column_display_pk = True
+
+admin.add_view(CourseView(Course, db.session, name="Course", endpoint='course_admin'))
+admin.add_view(StudentView(Student, db.session, name="Student", endpoint='student_admin'))
+admin.add_view(StudentCourseView(StudentCourseAssociation, db.session, name="Grades", endpoint='grades_admin'))
