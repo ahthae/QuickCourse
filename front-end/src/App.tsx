d073eb6a74d93b760236b4949016fa7df0f5888f
changnicholas64 <changnicholas64@gmail.com>
2026-04-17 14:03:28 -0700

    Barebones Viewing and Adding courses interface

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -76,13 +76,55 @@ function LoginPage() {
 function StudentDashboard() {
   const navigate = useNavigate()
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Your Courses</h1>
-      <p></p>
-      <button onClick={() => navigate('/')}>
-        Sign Out. Takes you back to Login
-      </button>
-    </div>
+    <section id="header">
+      <div style={{display: 'flex', justifyContent: 'flex-end', padding: '40px'}}>
+        <button onClick={() => navigate('/')}>
+           Sign Out. Takes you back to Login
+        </button>
+      </div>
+      <div style={{textAlign: "center" }}>
+        <h1>Your Courses</h1>
+        <table>
+            <tr>
+              <th>Course Name</th>
+              <th>Teacher</th>
+              <th>Time</th>
+              <th>Course Capacity</th>
+            </tr>
+            <tr></tr>
+        </table>
+      </div>
+      <section id='footer'>
+        <div style={{display: 'flex', justifyContent: 'flex-end', padding: '40px'}}>
+         <button onClick={() => navigate('/studentaddcourses')}>
+            Add Courses
+          </button>
+        </div>
+      </section>
+    </section>  
+  )
+}
+
+function StudentAddCourses() {
+  const navigate = useNavigate()
+  return (
+    <section id="header">
+      <div style={{display: 'flex', justifyContent: 'flex-end', padding: '40px'}}>
+        <button onClick={() => navigate('/')}>
+           Sign Out. Takes you back to Login
+        </button>
+      </div>
+      <div style={{textAlign: "center" }}>
+        <h1>Add Courses</h1>
+      </div>
+      <section id='footer'>
+        <div style={{display: 'flex', justifyContent: 'flex-end', padding: '40px'}}>
+         <button onClick={() => navigate('/studentdashboard')}>
+            View Courses
+          </button>
+        </div>
+      </section>
+    </section>  
   )
 }
 
@@ -118,6 +160,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/studentdashboard" element={<StudentDashboard />} />
+        <Route path="/studentaddcourses" element={<StudentAddCourses />} />
         <Route path="/teacherdashboard" element={<TeacherDashboard />} />
         <Route path="/admindashboard" element={<AdminDashboard />} />
       </Routes>
